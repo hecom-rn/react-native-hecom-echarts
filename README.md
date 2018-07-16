05899b609d679cff2b9758f9831feb03f57bf435
Xiaosong Gao <gaoxiaosong06@gmail.com>
2018-07-16 16:05:20 +0800

    初始化工程。

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # react-native-hecom-echarts
+
+[![Build Status](https://travis-ci.org/RNCommon/react-native-hecom-echarts.svg?branch=master)](https://travis-ci.org/RNCommon/react-native-hecom-echarts)
+
 Echarts报表的封装库。
+
+### 设置iOS
+
+在打包过程中拷贝资源的时候，需要添加如下的脚本命令：
+
+```
+echartsSrcPath='node_modules/react-native-hecom-echarts/src/echarts.js'
+echartsHtmlPath='node_modules/react-native-hecom-echarts/src/tpl.html'
+# jsbundle打包过程...
+cp $echartsSrcPath "[ResourcePath]/assets/${echartsSrcPath}"
+cp $echartsHtmlPath "[ResourcePath]/assets/${echartsHtmlPath}"
+```
+
+### 设置Android
+
+TODO
---
+++ src/index.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { View, WebView, Platform } from 'react-native';
+import PropTypes from 'prop-types';
+
+export default class Echart extends React.Component {
+    static propTypes = {
+        option: PropTypes.object.isRequired,
+        onPress: PropTypes.func,
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            height: 200,
+        };
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.option !== this.props.option) {
+            this.chart.reload();
+        }
+    }
+
+    setOption = (option) => {
+        this.chart.postMessage(JSON.stringify({setOption: option}));
+    };
+
+    render() {
+        return (
+            <View
+                style={[{flex: 1, backgroundColor: 'transparent'}, this.props.style]}
+                onLayout={event => {
+                    const height = event.nativeEvent.layout.height;
+                    if (this.state.height !== height) {
+                        this.setState({height});
+                    }
+                }}
+            >
+                <WebView
+                    bounces={false}
+                    ref={ref => this.chart = ref}
+                    injectedJavaScript={Echart.renderChart(this.props.option, this.state.height)}
+                    scalesPageToFit={Platform.OS === 'android'}
+                    source={Platform.OS === 'ios' ? require('./tpl.html') : {uri: 'file:///android_asset/echarts/tpl.html'}}
+                    onMessage={event => this.props.onPress ? this.props.onPress(JSON.parse(event.nativeEvent.data)) : null}
+                />
+            </View>
+        );
+    }
+
+    /**
+     * 该函数可以序列化function
+     */
+    static toString(obj) {
+        const demo = '~--demo--~';
+        let result = JSON.stringify(obj, function (key, val) {
+            if (typeof val === 'function') {
+                return `${demo}${val}${demo}`;
+            }
+            return val;
+        });
+        do {
+            // 最后一个replace将release模式中莫名生成的\"转换成"
+            result = result
+                .replace('\"~--demo--~', '')
+                .replace('~--demo--~\"', '')
+                .replace(/\\n/g, '')
+                .replace(/\\\"/g, '\"');
+        } while (result.indexOf(demo) >= 0);
+        return result;
+    }
+
+    static renderChart(option, height) {
+        return `
+            document.getElementById('main').style.width = 'auto';
+            document.getElementById('main').style.height = '${height}px';
+            var myChart = echarts.init(document.getElementById('main'));
+            var optionStr = ${Echart.toString(option)};
+            myChart.setOption(optionStr);
+            myChart.on('click', function(param) {
+                var seen = [];
+                var paramsString = JSON.stringify(params, function(key, val) {
+                    if (val != null && typeof val == 'object') {
+                        if (seen.indexOf(val) >= 0) {
+                            return;
+                        }
+                        seen.push(val);
+                    }
+                    return val;
+                });
+                window.postMessage(paramsString);
+            });
+        `;
+    }
+}
