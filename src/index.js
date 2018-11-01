4ff0668ab73cbe3dc758c0b8cf83824d6372282b
Ma Shuai <mashuai_hy@163.com>
2018-11-01 15:57:08 +0800

    fix format

--- src/index.js
+++ src/index.js
@@ -53,12 +53,12 @@ export default class Echart extends React.Component {
     }
 
     static toString(obj) {
-        return JSON.stringify(obj, function(key, val) {
-          if (typeof val === 'function') {
-            return `~--demo--~${val}~--demo--~`;
-          }
-          return val;
-        }).replace('\"~--demo--~', '').replace('~--demo--~\"', '').replace(/\\n/g, '').replace(/\\\"/g,"\"");//最后一个replace将release模式中莫名生成的\"转换成"
+        return JSON.stringify(obj, function(val) {
+            if (typeof val === 'function') {
+                return `~--demo--~${val}~--demo--~`;
+            }
+            return val;
+        }).replace('\"~--demo--~', '').replace('~--demo--~\"', '').replace(/\\n/g, '').replace(/\\\"/g,"\"");
     } 
 
     static renderChart(option, height) {
